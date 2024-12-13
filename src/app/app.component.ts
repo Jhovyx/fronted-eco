592818c590c79557ce331a740c661bf16f3ed32e
Hilder <hilderarce566@gmail.com>
2024-12-13 14:03:15 -0500

    avance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from './shared/services/user.service';
-import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  constructor(private router: Router, private userService: UserService) {}
-
-  ngOnInit(): void {
-    const user = this.userService.getCookie('user');
-    if(user?.userType === 'admin'){
-      this.router.navigate(['/admin-viajes']);
-    }
-  }
-}
+export class AppComponent {}
